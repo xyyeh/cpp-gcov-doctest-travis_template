3efdd0031098cab53887661fa7674ea49a1c3bd7
Xiyang Yeh <xiyang.yeh@flexivrobotics.com>
2020-04-08 21:16:33 -0700

    initial setup

--- app/main.cpp
+++ app/main.cpp
@@ -6,27 +6,23 @@
 #include "doctest.h"
 #endif
 
-#include <iostream>
 #include <stdlib.h>
 
-#include "exampleConfig.h"
+#include <iostream>
+
 #include "example.h"
+#include "exampleConfig.h"
 
 /*
  * Simple main program that demontrates how access
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << "."
-            << PROJECT_VERSION_PATCH
-            << "."
-            << PROJECT_VERSION_TWEAK
-            << std::endl;
-  std::system("cat ../LICENSE");
+  std::cout << "C++ Boiler Plate v" << PROJECT_VERSION_MAJOR << "."
+            << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH << "."
+            << PROJECT_VERSION_TWEAK << std::endl;
+
+  std::cout << "Initial testing" << std::endl;
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
